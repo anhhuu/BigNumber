8932ae398ddfc8418c64dff38cc428fa520f0aab
anhhuu <panhhuu@gmail.com>
2020-04-25 22:22:48 +0700

    Add test main

--- BigNumber/src/main.cpp
+++ BigNumber/src/main.cpp
@@ -7,25 +7,13 @@
 
 void test_394()
 {
-	while (true)
-	{
-		std::string num = "";
-		std::cin >> num;
-		std:: cout << Convert::CovertNumStringToBin(num) << std::endl;
-	}
+	Qint a;
+	a.ScanQInt();
 }
 
 int main()
 {
 	test_394();
-	std::string bits = "00001010100010010101111010101001010101001001010101000100101011110101010010101010010010101010001001010111101010100100000000000001111";
-	unsigned char data[16] = { 0 };
-	
-	BitProcess::SetBit(data, bits);
-	std::cout << BitProcess::GetBit(data);
-
-	std::cout << std::endl;
-
 	system("pause");
 	return 0;
 }
