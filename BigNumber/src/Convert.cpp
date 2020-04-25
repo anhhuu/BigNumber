52bff50442eb5490e9a4038ee6201be068b47a63
anhhuu <panhhuu@gmail.com>
2020-04-25 22:21:52 +0700

    Add method: convert bits to two complement bits

--- BigNumber/src/Convert.cpp
+++ BigNumber/src/Convert.cpp
@@ -1,10 +1,13 @@
 #include <algorithm>
 #include "Convert.h"
 #include "Utils.h"
+#include "BitProcess.h"
 
 std::string Convert::CovertNumStringToBin(std::string num)
 {
     std::string result;
+    unsigned int index = 0;
+    bool negative = num[0] == '-';    
 
     while (num != "") 
     {
@@ -14,10 +17,26 @@ std::string Convert::CovertNumStringToBin(std::string num)
 
     std::reverse(result.begin(), result.end());
 
+    BitProcess::StandardBits(result, 128);
+
+    if (negative)
+    {
+        ConvertBitsToTwoComplement(result, true);
+    }
+
     return result;
 }
 
 std::string Convert::CovertBinToNumString(std::string bits)
 {
 	return std::string();
+}
+
+void Convert::ConvertBitsToTwoComplement(std::string &bits, bool sign)
+{
+    if (sign)
+    {
+        BitProcess::ReverseBits(bits);
+        bits = BitProcess::AddTwoBits(bits, "1");
+    }
 }
