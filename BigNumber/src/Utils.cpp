2ffe99c0ab7dfe90a34582fe06762da77a6260bc
anhhuu <panhhuu@gmail.com>
2020-04-25 20:47:18 +0700

    Add method div for 2 at Utils class

--- BigNumber/src/Utils.cpp
+++ BigNumber/src/Utils.cpp
@@ -7,7 +7,40 @@ std::string Utils::AddTwoIntString(std::string num1, std::string num2)
 
 std::string Utils::DivideNumStringForTwo(std::string num)
 {
-	return std::string();
+	unsigned int index = 0;
+	if (num[0] == '-' || num[0] == '+')
+	{
+		index++;
+	}
+	unsigned int dividendNum = 0;
+	unsigned int surplus = 0;
+	unsigned int resultTemp = 0;
+	std::string resultStr = "";
+
+	while (index < num.length())
+	{
+		dividendNum = (dividendNum * 10) + num[index] - '0';
+		
+		resultTemp = dividendNum / 2;
+		surplus = dividendNum % 2;
+
+		if (surplus != 0)
+		{
+			dividendNum = surplus;
+		}
+		else
+		{
+			dividendNum = 0;
+		}
+		resultStr += std::to_string(resultTemp);
+		index++;
+	}
+
+	if (resultStr[0] == '0')
+	{
+		resultStr.erase(0, 1);
+	}
+	return resultStr;
 }
 
 std::string Utils::MultiplyNumStringWithOneDigit(std::string num1, int num2)
@@ -24,4 +57,3 @@ std::string Utils::PowOneDigit(int factor, int exp)
 {
 	return std::string();
 }
-
